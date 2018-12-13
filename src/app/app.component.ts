17cf28620ca3689a12c4ef62ff5218016732185e
Reshma Konepalli <rkonepalli@tesla.com>
2018-12-13 11:28:18 -0800

    service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from "./student.model";
+import { StudentService } from './student.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'services';
+export class AppComponent implements OnInit {
+
+  students: Student[] =[];
+
+constructor(private studentservice : StudentService){}
+  
+  ngOnInit(){
+
+    const studentObservable = this.studentservice.getStudents();
+    studentObservable.subscribe((studentsData: Student[]) =>{
+          this.students = studentsData;
+    });
+
+  }
+
+  getNames(){}
+
+  getServices(){}
 }
+
